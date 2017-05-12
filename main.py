91fdc5daca6534e5511f4685988b5aca80970fcd
Adrian Campos <dev.adriancampos@live.com>
2017-05-12 15:31:24 -0700

    Cleaned up prints and menu

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def main():
         print("1. Demo encryption and decryption with a string and a random keypair")
         print("2. Demo encryption and decryption with an integer and specified length")
         print("3. Generate a keypair")
-        print("4. Encrypt")
-        print("5. Decrypt")
+        print("4. Encrypt string")
+        print("5. Decrypt string")
         print("6. Exit")
 
         choice = input()
@@ -28,20 +28,31 @@ def main():
         if choice == '1':
             # Demo encryption and decryption with a randomly generated keypair
             demo_rsa_string(input("Enter a string to encrypt and decrypt: "))
+            print()
         elif choice == '2':
             try:
-                demo_rsa_with_integer(int(input("Enter integer message: ")), length=int(input("Enter length: ")))
+                demo_rsa_with_integer(int(input("Enter integer message: ")), key_length=int(input("Enter length: ")))
             except ValueError as e:
                 print("Couldn't test (" + str(e) + ")")
+            finally:
+                print()
         # Generate a keypair
         elif choice == '3':
             try:
                 n, e, d = rsa.generate_keys(int(input("Enter desired length: ")))
-                print("Modulus (n):\t" + str(n))
-                print("Public  (e):\t" + str(e))
-                print("Private (d):\t" + str(d))
+
+                print("----PUBLIC----")
+                print("Modulus     (n):\t" + str(n))
+                print("Public exp  (e):\t" + str(e))
+                print()
+
+                print("----PRIVATE---")
+                print("Modulus     (n):\t" + str(n))
+                print("Private exp (d):\t" + str(d))
             except ValueError as e:
                 print("Couldn't generate key (" + str(e) + ")")
+            finally:
+                print()
         # Encrypt
         elif choice == '4':
             try:
@@ -50,11 +61,13 @@ def main():
                     rsa.encrypt_string_by_parts(
                         input("Input message: "),
                         int(input("Enter the modulus    (n): ")),
-                        int(input("Enter the public key (e): "))
+                        int(input("Enter the public exp (e): "))
                     ))
             # In the event that the user enters a key that doesn't work, alert them
             except ValueError as e:
                 print("Invalid key (" + str(e) + ")")
+            finally:
+                print()
         # Decrypt
         elif choice == '5':
             try:
@@ -63,19 +76,21 @@ def main():
                     rsa.decrypt_string_by_parts(
                         input("Input ciphertext: "),
                         int(input("Enter the modulus     (n): ")),
-                        int(input("Enter the private key (d): "))
+                        int(input("Enter the private exp (d): "))
                     ))
             # In the event that the user enters a key that doesn't work, alert them
             except ValueError as e:
                 print("Invalid key (" + str(e) + ")")
+            finally:
+                print()
         # Exit
         elif choice == '6':
             break
 
 
-def demo_rsa_with_integer(message, length):
+def demo_rsa_with_integer(message, key_length):
     # Generate our keys
-    n, e, d = rsa.generate_keys(length)
+    n, e, d = rsa.generate_keys(key_length)
 
     print("n:\t" + str(n))
     print("e:\t" + str(e))
@@ -83,32 +98,32 @@ def demo_rsa_with_integer(message, length):
 
     print("Original message: " + str(message))
 
+    print("-----ENCRYPTION-----")
     print("Encrypting {} with public key: n={} | e={}:".format(message, n, e))
-
     # Encrypt using public key
     ciphertext = rsa.encrypt(message, n, e)
-
-    print("ciphertext: " + str(ciphertext))
-
-    print("Decrypting {} with private key: d={} | n={}:".format(ciphertext, d, n))
-
-    # Decrypt using private key
-    decrypted_message = rsa.decrypt(ciphertext, d, n)
-
-    print("decrypted: " + str(decrypted_message))
+    print()
+    print("Encrypted integer:\n" + str(ciphertext))
     print()
 
+    print("-----DECRYPTION-----")
+    print("Decrypting {} with private key: d={} | n={}:".format(ciphertext, d, n))
+    # Decrypt using private key
+    decrypted_message = rsa.decrypt(ciphertext, d, n)
+    print()
+    print("Decryptetd integer:\n" + str(decrypted_message))
 
-def demo_rsa_string(message, length=6):
+
+def demo_rsa_string(message, key_length=6):
     """
     Generates a keypair, encrypts message with n and e, and decrypts ciphertext with n and d
     :param message: 
-    :param length: 
+    :param key_length: 
     :return: 
     """
     # Generate keys
     print("-----GENERATING KEYS-----")
-    n, e, d = rsa.generate_keys(length)
+    n, e, d = rsa.generate_keys(key_length)
     print()
     print("Keys:")
     print("n:\t" + str(n))
